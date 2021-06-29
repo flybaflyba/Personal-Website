aac97ec381bbf4e4136216ea78e7cc742351bfde
“Litian <litian_zhang17@163.com>
2021-06-29 11:01:17 +0800

    Add timeline to homepage

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:timelines/timelines.dart';
 
 class Home extends StatefulWidget{
 
@@ -12,7 +13,110 @@ class _HomeState extends State<Home>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Home"),),
+      body: Center(
+        child:
+        Wrap(
+          children: [
+            // Column(
+            //   children: [
+            //     Text('ALOHA, I am'),
+            //     Text('Litian Zhang'),
+            //     Text('I am a college student currently studying computer science and entrepreneurship in Brigham Young University Hawaii '),
+            //   ],
+            // ),
+            TimelineTile(
+              oppositeContents: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(''),
+              ),
+              contents: Card(
+                child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text('Today'),
+                ),
+              ),
+              node: TimelineNode(
+                indicator: DotIndicator(),
+                startConnector: SolidLineConnector(),
+                endConnector: SolidLineConnector(),
+              ),
+            ),
+            SizedBox(
+              height: 80.0,
+              child: TimelineNode(
+                indicator: Card(
+                  margin: EdgeInsets.zero,
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Text('Aloha, I am Litian Zhang'),
+                        Text('I am an incoming associate support engineer at SAP China'),
+                      ],
+                    ),
+                  ),
+                ),
+                startConnector: DashedLineConnector(),
+                endConnector: SolidLineConnector(),
+              ),
+            ),
+            TimelineTile(
+              oppositeContents: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(''),
+              ),
+              contents: Card(
+                child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text('2021'),
+                ),
+              ),
+              node: TimelineNode(
+                indicator: DotIndicator(),
+                startConnector: SolidLineConnector(),
+                endConnector: SolidLineConnector(),
+              ),
+            ),
+            SizedBox(
+              height: 80.0,
+              child: TimelineNode(
+                indicator: Card(
+                  margin: EdgeInsets.zero,
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Text('Aloha, I am Litian Zhang'),
+                        Text('I am a college student studying computer science, information system, and entrepreneurship at Brigham Young University Hawaii.'),
+                      ],
+                    ),
+                  ),
+                ),
+                startConnector: DashedLineConnector(),
+                endConnector: SolidLineConnector(),
+              ),
+            ),
+            TimelineTile(
+              oppositeContents: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(''),
+              ),
+              contents: Card(
+                child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text('2018'),
+                ),
+              ),
+              node: TimelineNode(
+                indicator: DotIndicator(),
+                startConnector: SolidLineConnector(),
+                endConnector: SolidLineConnector(),
+              ),
+            ),
+          ],
+        )
+
+      )
     );
   }
 }
